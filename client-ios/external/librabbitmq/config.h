5ecec49bd5fdeaf72da19c216b2c22edf069951b
Adam Watson <adam@adamlwatson.com>
2011-11-25 21:29:48 -0800

    removing subdirs to add them back as submodules.

--- client-ios/external/librabbitmq/config.h
+++
@@ -1,4 +0,0 @@
-#define VERSION "0.0.1"
-#ifndef __cplusplus
-# define inline inline
-#endif // __cplusplus
--- server/config/deploy/staging.rb
+++
@@ -1,8 +0,0 @@
-#set :shell, "/bin/bash"
-#default_run_options[:shell]=false
-#of6paiGa
-
-role  :app, "node001.ext.alwlabs.com"
-set   :env, "development"
-set   :branch, 'master'
-
